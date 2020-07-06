93470949e680a2dadd589f86e5d76a5b417446c8
sctrandrei <sctr.andrei@gmail.com>
2020-07-06 20:44:52 +0300

    updates

---
+++ week01/Week01ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week01ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
