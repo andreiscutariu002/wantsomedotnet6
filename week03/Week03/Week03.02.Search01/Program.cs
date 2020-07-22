f030e7703569845636b767a71dde03f107eb3b07
sctrandrei <sctr.andrei@gmail.com>
2020-07-22 19:06:51 +0300

    updates

---
+++ week03/Week03/Week03.02.Search01/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Week03._02.Search01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new[] { 1, 6, 4, 5, 2, 0, -1 };
+
+            int valueToSearch = 1;
+
+            // parcurgem array
+            // fiecare cu valueToSearch
+            // am nevoie de n interatii
+
+            // Linear Search 
+            // un array - start => end (N pasi) =>
+
+            // complexitate 
+            // daca avem dataset (are un nr N de elemente)
+            // daca il parcurgem de la start => final => O(N)
+            // o cautare liniara are complixate O(N)
+
+            // fiecare element odata din dataset => O(N)
+            // fiecare element cu fiecare element => O(N*N) - O(N) la patrat
+        }
+
+        //Given an unsorted array which has a number in the majority (a number appears more than 50% in the array), find that number?
+        // pozitia unde avem element majoritar
+        // -1 in caz ca nu avem element majoritar
+        static int Ex6(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int count = 1;
+                
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        count++;
+                    }
+                }
+
+                if (count > array.Length / 2)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        static int[] Ex19MinMax(int[] arr)
+        {
+            int min1 = arr[0], min1pos = -1, min2 = arr[0], max1 = arr[0], max1pos = -1, max2 = arr[0], sumMin, sumMax;
+            int[] arrayMinMax = new int[2];
+            
+            for (int i = 1; i <= arr.Length - 1; i++)
+            {
+                if (arr[i] < min1)
+                {
+                    min1 = arr[i];
+                    min1pos = i;
+                }
+            }
+
+            for (int i = 1; i <= arr.Length - 1; i++)
+            {
+                if (arr[i] < min2 && min1pos != i)
+                {
+                    min2 = arr[i];
+                }
+            }
+            
+            for (int i = 1; i <= arr.Length - 1; i++)
+            {
+                if (arr[i] > max1)
+                {
+                    max1 = arr[i];
+                    max1pos = i;
+                }
+            }
+            
+            for (int i = 1; i <= arr.Length - 1; i++)
+            {
+                if (arr[i] > min2 && max1pos != i)
+                {
+                    max2 = arr[i];
+                }
+            }
+            
+            sumMin = min1 + min2;
+            sumMax = max1 + max2;
+            arrayMinMax[0] = sumMin;
+            arrayMinMax[1] = sumMax;
+            
+            return arrayMinMax;
+        }
+
+        static void DisplayArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++) 
+            for (int j = 0; j < array.Length; j++) 
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
