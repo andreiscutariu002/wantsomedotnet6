8692cb3d229f768dcb7fa45feedd39bd1b9a8b17
sctrandrei <sctr.andrei@gmail.com>
2020-08-12 18:58:10 +0300

    add more principles

---
+++ week05/Week05/Week05.OOP2.5_Encapsulation/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Week05.OOP2._5_Encapsulation
+{
+    using System.Collections.Generic;
+
+    public class User
+    {
+        private string userName;
+
+        private string password;
+
+        public string Password2 { get; private set; }
+
+        private List<string> oldPasswords;
+
+        public User(string userName, string password)
+        {
+            this.userName = userName;
+            this.password = password;
+            this.oldPasswords = new List<string>();
+        }
+
+        public void ChangePassword(string oldPassword, string newPassword)
+        {
+            if (CheckPassword(oldPassword))
+            {
+                StoreOldPassword(oldPassword);
+
+                this.password = newPassword;
+            }
+        }
+
+        private bool CheckPassword(string oldPassword)
+        {
+            if (this.password == oldPassword)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void StoreOldPassword(string oldPassword)
+        {
+            this.oldPasswords.Add(oldPassword);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User("1234", "2134");
+
+            user.ChangePassword("2134", "1234");
+
+            var p = user.Password2;
+
+            // error due to private set
+            //user.Password2 = "new password";
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ week05/Week05/Week05.OOP2.5_VirtualMembers/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Week05.OOP2._5_VirtualMembers
+{
+    // nu pot fi instantiate
+    public abstract class Employee
+    {
+        public string Cnp { get; set; }
+
+        public string Email { get; set; }
+
+        public virtual decimal Salary { get; set; }
+
+        // virtual - permin suprascrierea acestei metode in clase derivate
+        public virtual decimal CalculateSalary()
+        {
+            return this.Salary;
+        }
+
+        public abstract string GetEmployeeType();
+    }
+
+    public class Programmer : Employee
+    {
+        public string Language { get; set; }
+
+        public override decimal Salary { get; set; }
+
+        public override string GetEmployeeType()
+        {
+            return "Programmer";
+        }
+    }
+
+    public class Teacher : Employee
+    {
+        public string School { get; set; }
+
+        public override string GetEmployeeType()
+        {
+            return "Teacher";
+        }
+
+        public override decimal CalculateSalary()
+        {
+            return this.Salary * 0.9m;
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public override string GetEmployeeType()
+        {
+            return "Manager";
+        }
+
+        // suprascriere a metodei CalculateSalary
+        // un alt comportament definit
+        public override decimal CalculateSalary()
+        {
+            return this.Salary * 1.1m;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var programmer = new Programmer
+            {
+                Cnp = "1234",
+                Salary = 1234,
+                Email = "andrei@site.com",
+                Language = "C#"
+            };
+
+            var teacher = new Teacher
+            {
+                Cnp = "12344",
+                Salary = 12345,
+                Email = "dan@site.com",
+                School = "New York"
+            };
+
+            var manager = new Manager()
+            {
+                Cnp = "12346",
+                Salary = 1234500,
+                Email = "ion@site.com",
+            };
+
+            PrintEmployeeDetails(programmer);
+            PrintEmployeeDetails(teacher);
+            PrintEmployeeDetails(manager);
+        }
+
+        private static void PrintEmployeeDetails(Employee employee)
+        {
+            Console.WriteLine($"{employee.Email} - {employee.CalculateSalary()}");
+        }
+    }
+}
