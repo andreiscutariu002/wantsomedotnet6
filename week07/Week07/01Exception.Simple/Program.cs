7327ebbe72d6197c4f1cfc50bf38f2f5d7ebce07
sctrandrei <sctr.andrei@gmail.com>
2020-08-26 18:28:54 +0300

    update

--- week07/Week07/01Exception.Simple/Program.cs
+++ week07/Week07/01Exception.Simple/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
-
-namespace _01Exception.Simple
+﻿namespace _01Exception.Simple
 {
-    class Program
+    using System;
+
+    // Errors
+    // 1. Compile Time ( Visual Studio Error )
+    // 2. Runtime ( Exceptions )
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // compile error
+            // var i = 1;
+            // string s = i;
+
+            // possible runtime error
+            
+            try // avem cod care pontetial, poate arunca exceptii
+            {
+                var line = Console.ReadLine();
+                var number = int.Parse(line);
+            }
+            catch (Exception e) // handle (tratam) exceptii
+            {
+                Console.WriteLine("Error");
+                //Console.WriteLine(e);
+                //throw;
+            }
         }
     }
 }
