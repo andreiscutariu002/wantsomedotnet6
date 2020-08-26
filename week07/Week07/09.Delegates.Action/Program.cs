56daa0cbb3392ebc95eddcd4c0a7dca0f762bec6
sctrandrei <sctr.andrei@gmail.com>
2020-08-26 20:29:53 +0300

    update

---
+++ week07/Week07/09.Delegates.Action/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _09.Delegates.Action
+{
+    using Action = System.Action;
+
+    class Program
+    {
+        // Action is a delegate type defined in the System namespace. An Action type delegate is the same as Func delegate except that the Action delegate doesn't return a value. In other words, an Action delegate can be used with a method that has a void return type.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Action<string, int> m1 = Method1;
+
+            Action<string, int, double> m2 = Method2;
+
+            // inline 
+            Action<string, int, double> inline = (s, i, arg3) =>
+            {
+                //...
+            };
+
+            Action<string, int, double> inlineV2 = delegate(string s, int i, double arg3)
+            {
+                //...
+            };
+        }
+
+        static void Method1(string a, int b)
+        {
+        }
+
+        static void Method2(string a, int b, double c)
+        {
+        }
+    }
+}
---
+++ week07/Week07/09.Delegates.Predicate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _09.Delegates.Predicate
+{
+    //Predicate is the delegate like Func and Action delegates. It represents a method containing a set of criteria and checks whether the passed parameter meets those criteria. A predicate delegate methods must take one input parameter and return a boolean - true or false.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> predicate = IsUpperCase;
+
+            Predicate<int> isPositive = param =>
+            {
+                var isPos = param > 0;
+
+                return isPos;
+            };
+
+            bool res = isPositive(200);
+        }
+
+        static bool IsUpperCase(string str)
+        {
+            return str.Equals(str.ToUpper());
+        }
+    }
+}
