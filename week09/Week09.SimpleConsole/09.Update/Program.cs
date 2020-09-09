eb49b4ef012dc61994edf3ad2e5c2c512cc8de25
sctrandrei <sctr.andrei@gmail.com>
2020-09-09 20:35:57 +0300

    update

---
+++ week09/Week09.SimpleConsole/09.Update/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _09.Update
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server=.;Database=week09;Trusted_Connection=True;";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                try
+                {
+                    var id = int.Parse(Console.ReadLine());
+                    var name = Console.ReadLine();
+
+                    var commandText = $"update Publisher set name = @nameParam where publisherid = @idParam"; // avoid += concatenation
+                    var command = new SqlCommand(commandText);
+
+                    var idParamObject = new SqlParameter("idParam", id);
+                    var nameParamObject = new SqlParameter("nameParam", name);
+
+                    command.Parameters.Add(idParamObject);
+                    command.Parameters.Add(nameParamObject);
+
+                    command.Connection = connection;
+
+                    var result = command.ExecuteNonQuery();
+                    Console.WriteLine($"Response: {result}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ week09/Week09.SimpleConsole/10.Delete/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _10.Delete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server=.;Database=week09;Trusted_Connection=True;";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                try
+                {
+                    var id = int.Parse(Console.ReadLine());
+
+                    var commandText = $"delete from Publisher where publisherid = @idParam"; // avoid += concatenation
+                    var command = new SqlCommand(commandText);
+
+                    var idParamObject = new SqlParameter("idParam", id);
+
+                    command.Parameters.Add(idParamObject);
+
+                    command.Connection = connection;
+
+                    var result = command.ExecuteNonQuery();
+                    Console.WriteLine($"Response: {result}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
