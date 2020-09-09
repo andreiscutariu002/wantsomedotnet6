ae2b6ae579863bf8f6808e470d1c694c342f04fa
sctrandrei <sctr.andrei@gmail.com>
2020-09-09 20:30:35 +0300

    add insert

---
+++ week09/Week09.SimpleConsole/07.InsertSqlInjection/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _07.InsertSqlInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server=.;Database=week09;Trusted_Connection=True;";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                try
+                {
+                    var id = int.Parse(Console.ReadLine());
+                    var name = Console.ReadLine();
+
+                    // here we can have sql injection
+                    // caused by += concatenation to create a query
+                    var commandText = $"insert into Publisher values ({id}, {name})";
+                    var command = new SqlCommand(commandText);
+
+                    command.Connection = connection;
+
+                    var result = command.ExecuteNonQuery();
+                    Console.WriteLine($"Response: {result}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ week09/Week09.SimpleConsole/08.InsertUsingParams/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _08.InsertUsingParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server=.;Database=week09;Trusted_Connection=True;";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                try
+                {
+                    var id = int.Parse(Console.ReadLine());
+                    var name = Console.ReadLine();
+
+                    var commandText = $"insert into Publisher values (@idParam, @nameParam)"; // avoid += concatenation
+                    var command = new SqlCommand(commandText);
+
+                    var idParamObject = new SqlParameter("idParam", id);
+                    var nameParamObject = new SqlParameter("nameParam", name);
+
+                    command.Parameters.Add(idParamObject);
+                    command.Parameters.Add(nameParamObject);
+
+                    command.Connection = connection;
+
+                    var result = command.ExecuteNonQuery();
+                    Console.WriteLine($"Response: {result}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
