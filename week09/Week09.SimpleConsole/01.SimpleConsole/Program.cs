f9bbe4c65417bc8b97370cf02f09e68a3efb2da2
sctrandrei <sctr.andrei@gmail.com>
2020-09-09 19:36:15 +0300

    update

--- week09/Week09.SimpleConsole/01.SimpleConsole/Program.cs
+++ week09/Week09.SimpleConsole/01.SimpleConsole/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace _01.SimpleConsole
 {
+    // method to connect to database
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // un server name [ip]
+            // un database name
+            // detalii de autentificare
+                // user & password
+                // windows authentifiation [Trusted_Connection]
+            
+            var connectionString = @"Server=.;Database=week09;Trusted_Connection=True;";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();  
+            } catch (Exception e)
+            {
+                //
+
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
---
+++ week09/Week09.SimpleConsole/03.Insert/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _03.Insert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server=.;Database=week09;Trusted_Connection=True;";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+            
+            var commandText = "insert into Publisher values (5, 'Adrian')";
+            var command = new SqlCommand(commandText);
+
+            command.Connection = connection;
+
+            // un sql connection - o instructiune catre DB
+
+            // 3 moduri de interactiune cu DB-ul
+
+                // 1 - Query [citesti date] - astepti o lista ca raspuns 
+                // 2 - Non Query [scriu data] - trimit ceva la DB, nu astept raspuns
+                // 3 - Execute Scalar - trimit o comanda, astept un raspuns, ca un singur obiect
+                // Returns a maximum of 2033 characters
+
+            var result = command.ExecuteNonQuery(); // modul 2 [intoarce numarul de randuri afectate]
+            Console.WriteLine($"Response: {result}");
+        }
+    }
+}
